a9a374b7c878ad5ecad7ae24c1131834b010c304
Snizhana <melniksnizana78@gmail.com>
2023-04-28 14:32:07 +0300

    Homework_5_5

--- Homework_5_5.py
+++ Homework_5_5.py
@@ -1,9 +1,25 @@
 suma = int(input())
 s = [10,20,50,100,200,500,1000]
-for id, nom in enumerate(s):
-	while suma:
-		suma = suma % (nom * 10)
-		if suma < s[id + 1]:
-			
+suma_n = suma
 
-
+while suma_n: 
+  
+  for id, nom in enumerate(s):
+    
+    k = suma // nom 
+   
+    if k > 10:
+      k = 10
+     
+    elif k < 1:
+      print('no')
+      break
+    suma_n = suma - (nom * k)  
+    
+       
+    while suma_n % s[id + 1] :
+      
+      k -= 1
+      suma_n = suma - (nom * k)
+    suma = suma_n
+    print(k, str(nom))
